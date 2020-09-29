7af52f27eb8e7031bbe88b61407ca30183c4a81b
Survikrowa <survikrowa@o2.pl>
2020-09-29 17:25:43 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Button } from "reactstrap";
+import "./styles/main.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <Button color="primary">Click me</Button>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="h-12">im working</div>
+    </>
+  );
+};
