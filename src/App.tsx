dd3aa93d1f0b3e1cf3f278ab00c99a86868b33e4
Survikrowa <szymonhrabia60@gmail.com>
2020-11-16 21:24:39 +0100

    Add react query to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { QueryCache, ReactQueryCacheProvider } from "react-query";
 import "./app.scss";
 import { Home } from "./pages/Home";
-import { AppMenu } from "./components/AppMenu/AppMenu";
 import { AboutUs } from "./pages/AboutUs";
 import { Recruit } from "./pages/Recruit";
+import { PrivateRoute } from "./components/CustomRoutes/PrivateRoute";
+import { UserPanel } from "./pages/UserPanel";
+
+const queryCache = new QueryCache();
 
 function App() {
   return (
-    <Router>
-      <AppMenu />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={AboutUs} />
-        <Route exact path="/recruit" component={Recruit} />
-      </Switch>
-    </Router>
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={AboutUs} />
+          <Route exact path="/recruit" component={Recruit} />
+          <PrivateRoute path="/panel">
+            <UserPanel />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </ReactQueryCacheProvider>
   );
 }
 
